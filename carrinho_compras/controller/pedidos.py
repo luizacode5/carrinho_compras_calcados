b1def06db4ec9f1d6337722f5e37d39bccd0cbb1
Bruna Cotrim <bruna_viotto@hotmail.com>
2022-10-11 20:50:06 -0300

    feat: cria rotas, regras e persistencia de pedidos

---
+++ carrinho_compras/controller/pedidos.py
@@ -0,0 +1,126 @@
+
+from fastapi import HTTPException, status
+
+from carrinho_compras.schemas.pedidos import *
+from carrinho_compras.controller import uteis
+from carrinho_compras.persistence import pedidos
+
+
+async def insere_pedido(pedido: PedidoSchema):
+    resultado = await pedidos.insere_pedido(pedido)
+    return resultado
+
+async def busca_pedido_por_id(id_pedido: str):
+    resultado = await pedidos.busca_pedido_por_id(id_pedido)
+    return resultado
+
+async def busca_pedidos_por_cliente(
+    email_cliente: str, 
+    numero_pagina: int, 
+    qtde_por_pagina: int
+    ) -> ListaPedidos:
+
+    registros_pular, qtde_por_pagina = await uteis.ajusta_paginacao(
+        numero_pagina, 
+        qtde_por_pagina
+        )
+
+    lista_pedidos = await pedidos.busca_pedidos_por_cliente(
+        email_cliente, 
+        registros_pular, 
+        qtde_por_pagina
+        )
+
+    if len(lista_pedidos.pedidos) != 0:
+        if numero_pagina == 0:
+            numero_pagina = 1
+        lista_pedidos.numero_pagina = numero_pagina
+        lista_pedidos.qtde_por_pagina = qtde_por_pagina
+        return lista_pedidos
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail="Não foram encontrados pedidos para o cliente")
+
+async def busca_pedidos_por_produto(
+    codigo_produto: str,
+    cor_produto: str,
+    numeracao_produto: str,
+    numero_pagina: int,
+    qtde_por_pagina: int) -> ListaPedidos:
+
+    registros_pular, qtde_por_pagina = await uteis.ajusta_paginacao(
+        numero_pagina, 
+        qtde_por_pagina
+        )
+    
+    filtro_produto = await uteis.gera_filtro_produto(codigo_produto, cor_produto, numeracao_produto)
+
+    resultado = await pedidos.busca_pedidos_por_produto(
+        filtro_produto,
+        registros_pular, 
+        qtde_por_pagina
+        )
+    if numero_pagina == 0:
+        numero_pagina = 1
+    resultado.numero_pagina = numero_pagina
+    resultado.qtde_por_pagina = qtde_por_pagina
+
+    if resultado:
+        return resultado
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail="Não foram encontrados pedidos com o produto")
+
+async def busca_produtos_mais_vendidos(
+    numero_pagina: int, 
+    qtde_por_pagina: int
+    ) -> ProdutosMaisVendidos:
+
+    registros_pular, qtde_por_pagina = await uteis.ajusta_paginacao(
+        numero_pagina, 
+        qtde_por_pagina
+        )
+
+    resultado = await pedidos.busca_produtos_mais_vendidos(
+        registros_pular, 
+        qtde_por_pagina
+        )
+    if numero_pagina == 0:
+        numero_pagina = 1
+    resultado.numero_pagina = numero_pagina
+    resultado.qtde_por_pagina = qtde_por_pagina
+    
+    if resultado:
+        return resultado
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail="Não foram encontrados registros para os parâmetros especificados")
+    
+async def busca_total_pedidos_por_cliente(
+    numero_pagina: int, 
+    qtde_por_pagina: int,
+    ordenacao: str
+    ) -> TotalPedidoClientes:
+
+    registros_pular, qtde_por_pagina = await uteis.ajusta_paginacao(
+        numero_pagina, 
+        qtde_por_pagina
+        )
+
+    if ordenacao.upper() == "QUANTIDADE":
+        ordenacao_filtro = "quantidade_total"
+    elif ordenacao.upper() == "VALOR":
+        ordenacao_filtro = "valor_total"
+
+    resultado = await pedidos.busca_total_pedidos_por_cliente(
+        registros_pular, 
+        qtde_por_pagina,
+        ordenacao_filtro
+        )
+    if numero_pagina == 0:
+        numero_pagina = 1
+    resultado.numero_pagina = numero_pagina
+    resultado.qtde_por_pagina = qtde_por_pagina
+    
+    if resultado:
+        return resultado
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail="Não foram encontrados registros para os parâmetros especificados")
---
+++ carrinho_compras/persistence/pedidos.py
@@ -0,0 +1,160 @@
+from fastapi import HTTPException, status
+import logging
+from bson import ObjectId
+from pymongo import DESCENDING
+
+from carrinho_compras.schemas.pedidos import *
+from carrinho_compras.persistence.persistence_bd import obter_colecao
+
+
+COLECAO_PEDIDOS = obter_colecao("pedido")
+
+async def insere_pedido(pedido: PedidoSchema):
+    try:
+        dados_pedido = pedido.dict()
+
+        resultado = await COLECAO_PEDIDOS.insert_one(dados_pedido)
+
+        if resultado.inserted_id:
+            return resultado.inserted_id
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Não foi possível fechar o pedido")
+
+    except Exception as e:
+        logging.exception(f'insere_pedido.erro: {e}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail="Não foi possível inserir o pedido")
+
+async def busca_pedido_por_id(id_pedido: str) -> PedidoSchema:
+    try:
+        resultado = await COLECAO_PEDIDOS.find_one(
+            {"_id" : ObjectId(id_pedido)}
+        )
+
+        if resultado:
+            pedido = PedidoSchema(**resultado)
+            pedido.id_pedido = str(resultado["_id"])
+            return pedido
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+            detail="O pedido não foi encontrado")
+
+    except Exception as e:
+        logging.exception(f'busca_pedido_por_id.erro: {e}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail="Não foi possível consultar o pedido")
+
+
+async def busca_pedidos_por_cliente(
+    email_cliente: str,
+    registros_pular: int,
+    qtde_por_pagina: int
+    ) -> ListaPedidos:
+    try:
+        lista_pedidos = ListaPedidos()
+
+        cursor_pesquisa = COLECAO_PEDIDOS \
+            .find({"email_cliente": email_cliente}) \
+            .skip(registros_pular) \
+            .limit(qtde_por_pagina) \
+            .sort("data_criacao_pedido", DESCENDING)
+
+        async for pedidos_resultado in cursor_pesquisa:
+            pedido = PedidoSchema(**pedidos_resultado)
+            pedido.id_pedido = str(pedidos_resultado["_id"])
+            lista_pedidos.pedidos.append(pedido)
+
+        return lista_pedidos
+
+    except Exception as e:
+        logging.exception(f'busca_pedidos_por_cliente.erro: {e}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail="Não foi possível realizar a consulta de pedidos por cliente")
+
+async def busca_pedidos_por_produto(
+    filtro_produto: dict, 
+    registros_pular: int, 
+    qtde_por_pagina: int
+    ) -> ListaPedidos:
+    try:
+        lista_pedidos = ListaPedidos()
+
+        cursor_pesquisa = COLECAO_PEDIDOS \
+            .find({"produtos": 
+                        {"$elemMatch": filtro_produto}}) \
+            .skip(registros_pular) \
+            .limit(qtde_por_pagina) \
+            .sort("data_criacao_pedido", DESCENDING)
+
+        async for pedidos_resultado in cursor_pesquisa:
+            pedido = PedidoSchema(**pedidos_resultado)
+            lista_pedidos.pedidos.append(pedido)
+
+        return lista_pedidos
+
+    except Exception as e:
+        logging.exception(f'busca_pedidos_por_produto.erro: {e}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail="Não foi possível realizar a consulta de pedidos por produto")
+
+async def busca_produtos_mais_vendidos(
+    registros_pular: int,
+    qtde_por_pagina: int
+    ) -> ProdutosMaisVendidos:
+    try:
+        lista_produtos = ProdutosMaisVendidos()
+        lista_produtos.produtos = []
+
+        cursor_pesquisa = COLECAO_PEDIDOS \
+            .aggregate( [   
+                    {"$unwind": "$produtos" }, 
+                    {"$group": {"_id": {"codigo": "$produtos.codigo",  
+                                        "cor": "$produtos.cor", 
+                                        "numeracao": "$produtos.numeracao"},  
+                    "quantidade_total": {"$sum": "$produtos.quantidade" }}}, 
+                    {"$sort":{"quantidade_total": DESCENDING}},
+                    {"$skip":registros_pular},
+                    {"$limit":qtde_por_pagina}
+            ])
+
+        async for produtos_resultado in cursor_pesquisa:
+            produto = QuantidadeTotalPorProduto(**produtos_resultado["_id"])
+            produto.quantidade_total = produtos_resultado["quantidade_total"]
+            lista_produtos.produtos.append(produto)
+
+        return lista_produtos
+    except Exception as e:
+        logging.exception(f'busca_produtos_mais_vendidos.erro: {e}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail="Não foi possível realizar a consulta de produtos mais vendidos")
+
+async def busca_total_pedidos_por_cliente(
+    registros_pular: int,
+    qtde_por_pagina: int,
+    ordenacao_filtro: str
+    ) -> TotalPedidoClientes:
+    try:
+        lista_clientes = TotalPedidoClientes()
+        lista_clientes.clientes = []
+
+        cursor_pesquisa = COLECAO_PEDIDOS \
+            .aggregate( [
+                    {"$group": {"_id": "$email_cliente",  
+                    "quantidade_total": {"$count": {}},
+                    "valor_total": {"$sum": "$valor_total"}}}, 
+                    {"$sort":{ordenacao_filtro: DESCENDING}},
+                    {"$skip":registros_pular},
+                    {"$limit":qtde_por_pagina}
+            ] )
+
+        async for clientes_resultado in cursor_pesquisa:
+            cliente = TotalPedidosPorCliente()
+            cliente.email_cliente = clientes_resultado["_id"]
+            cliente.quantidade_total = round(clientes_resultado["quantidade_total"], 2)
+            cliente.valor_total = round(clientes_resultado["valor_total"], 2)
+            lista_clientes.clientes.append(cliente)
+
+        return lista_clientes
+    except Exception as e:
+        logging.exception(f'busca_total_pedidos_por_cliente.erro: {e}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail="Não foi possível realizar a consulta de totais de pedidos por cliente")

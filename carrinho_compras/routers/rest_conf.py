9903d8b5cc082412a1c78d1a81a72429cd8dcc92
Bruna Cotrim <bruna_viotto@hotmail.com>
2022-10-11 20:47:09 -0300

    feat: adiciona rota de pedidos na configuração da api

--- carrinho_compras/routers/rest_conf.py
+++ carrinho_compras/routers/rest_conf.py
@@ -4,6 +4,7 @@ from carrinho_compras.routers.principal import rota_principal
 from carrinho_compras.routers.clientes import rota_clientes
 from carrinho_compras.routers.produtos import rota_produtos
 from carrinho_compras.routers.carrinhos import rota_carrinhos
+from carrinho_compras.routers.pedidos import rota_pedidos
 
 
 def configurar_rotas(app: FastAPI):
@@ -11,6 +12,7 @@ def configurar_rotas(app: FastAPI):
     app.include_router(rota_clientes)
     app.include_router(rota_produtos)
     app.include_router(rota_carrinhos)
+    app.include_router(rota_pedidos)
 
 def criar_aplicacao_fastapi():
     descricao_api = "Esta API é responsável pelos carrinhos de compra, cadastro \
